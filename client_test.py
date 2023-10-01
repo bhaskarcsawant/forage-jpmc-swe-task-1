9a51c2d410da37e4d9e4c6762eb379967b315c82
Bhaskar Sawant <bhaskar.csawant417@gmail.com>
2023-10-01 18:01:04 +0530

    JPMC-SWE Task 1

--- client_test.py
+++ client_test.py
@@ -6,6 +6,7 @@ class ClientTest(unittest.TestCase):
     quotes = [
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+      # {'top_ask': {'price': 122.68, 'size': 6}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.87, 'size': 78}, 'id': '0.109974697771', 'stock': 'GHI'},
     ]
     """ ------------ Add the assertion below ------------ """
 
@@ -13,9 +14,11 @@ class ClientTest(unittest.TestCase):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+      # {'top_ask': {'price': 119.68, 'size': 6}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.87, 'size': 78}, 'id': '0.109974697771', 'stock': 'GHI'},
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote), (quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   """ ------------ Add more unit tests ------------ """
 
